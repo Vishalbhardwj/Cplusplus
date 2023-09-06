3e4896a27692ee0c794314f92e9fbbbcf33b06d1
Vishalbhardwj <139758910+Vishalbhardwj@users.noreply.github.com>
2023-09-06 11:42:10 +0000

    Adding Question

--- Question(IMP)/01Array/11FindDuplicate.cpp
+++ Question(IMP)/01Array/11FindDuplicate.cpp
@@ -1,3 +1,44 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+int duplicateNum(vector<int> &a){
+    int slow =a[0],fast=a[0];
+    do{
+        slow=a[slow];
+        fast=a[a[fast]];
+    }while(slow!=fast);
+
+    slow=a[0];
+    while(slow!=fast){
+        slow=a[slow];
+        fast=a[fast];
+    }
+    return slow;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        
+        for(int i=0;i<n;i++){
+        int k;
+        cin>>k;
+        v[i]=k;
+        }
+        for(auto x:v){
+            cout<<x;
+        }
+        cout<<endl;
+        int value=duplicateNum(v);
+        cout<<value<<endl;
+        
+    }
+    return 0;
+}
+
 
