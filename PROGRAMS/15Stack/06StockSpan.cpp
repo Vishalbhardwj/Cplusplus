118abec65d747cb22af375462212ed265173a64e
Vishalbhardwj <139758910+Vishalbhardwj@users.noreply.github.com>
2023-10-06 16:58:04 +0000

    stack Code(StockSpan)

---
+++ PROGRAMS/15Stack/06StockSpan.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void stockSpan(int arr[],int n){
+    stack<int> s;
+    s.push(0);
+    cout<<"1"<<" ";
+    for(int i=1;i<n;i++){
+        while(s.empty()==false&&arr[s.top()]<=arr[i]){
+            s.pop();
+        }
+        int span=1;
+        if(s.empty()){
+            span=i+1;
+        }else{
+            span=i-s.top();
+        }
+        cout<<span<<" ";
+        s.push(i);
+    }
+}
+
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        stockSpan(arr,n);
+
+    }
+    return 0;
+}
